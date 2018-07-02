72603b3eed178a4950cdc17c9fff03f3aa57bd74
Taras <tarasserhiytschuk@gmail.com>
2018-07-02 15:35:03 +0300

    rm unused

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
   <div id="app">
-    <MenuMiTouchStandard/>
   </div>
 </template>
 
 <script>
-import MenuMiTouchStandard from './components/mt-menu.vue'
 
 export default {
   name: 'app',
-  components: {
-      MenuMiTouchStandard
-  }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
